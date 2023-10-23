7078763b71235040961056e93a086fc8bb7f645c
Destinee A. Adams <adestinee37@gmail.com>
2023-10-23 19:32:34 -0400

    -

--- ifs/practice.py
+++ ifs/practice.py
@@ -5,4 +5,20 @@ obtained by a student.
     if the percentage is above 90, assign grade A
     if the percentage is above 75, assign grade B
     if the percentage is above 65, assign grade C
+    anything else is an F
 '''
+
+grade = 90
+
+if  grade >= 90:
+  print("your grade is A")
+  
+elif grade >= 75:
+  print("your grade is B")
+  
+elif condition:
+  pass
+  
+  
+else:
+  pass
